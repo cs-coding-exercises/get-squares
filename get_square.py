bfc6d2144bfb1ada2114932db966a965f3714391
cs-coding-exercises <37808365+cs-coding-exercises@users.noreply.github.com>
2018-04-05 11:39:18 -0400

    Update get_square.py

--- get_square.py
+++ get_square.py
@@ -8,6 +8,8 @@ def get_encomapsing_square(file_name):
     Upper_Y    = None
 
     with open(file_name, 'r') as file:
+        print("\n\nFile: " + file_name)
+        print("-------------------------------")
         for line in file:
             square_data_arr = line.split()
             print("square_data: " + str(square_data_arr))
@@ -18,10 +20,10 @@ def get_encomapsing_square(file_name):
             low_y = int(square_data_arr[1])
             upp_y = int(square_data_arr[1]) + int(square_data_arr[3])
 
-            print("low_x: " + str(low_x) )
-            print("upp_x: " + str(upp_x) )
-            print("low_y: " + str(low_y) )
-            print("upp_y: " + str(upp_y) )
+            #print("low_x: " + str(low_x) )
+            #print("upp_x: " + str(upp_x) )
+            #print("low_y: " + str(low_y) )
+            #print("upp_y: " + str(upp_y) )
 
             if Lower_X is None:   Lower_X = low_x
             elif low_x < Lower_X: Lower_X = low_x
@@ -35,16 +37,17 @@ def get_encomapsing_square(file_name):
             if Upper_Y is None:   Upper_Y = upp_y
             elif upp_y > Upper_Y: Upper_Y = upp_y
 
-        print("==============================")
+        print("-------------------------------")
         print("Lower_X: " + str(Lower_X) )
         print("Lower_Y: " + str(Lower_Y) )
         print("Upper_X: " + str(Upper_X) )
         print("Upper_Y: " + str(Upper_Y) )
-        print("==============================")
+        print("-------------------------------")
         print("Bottom Left  : " + str(Lower_X) + "," + str(Lower_Y) )
         print("Bottom Right : " + str(Upper_X) + "," + str(Lower_Y) )
         print("Top Left     : " + str(Lower_X) + "," + str(Upper_Y) )
         print("Top Right    : " + str(Upper_X) + "," + str(Upper_Y) )
+        print("==============================")
 
 
 ##===============================
@@ -53,6 +56,6 @@ files=[]
 for x in range (1 , argv_len):
     file[x-1] = sys.argv[x]
 
-#files = ['squares_1.dat','squares_2.dat','squares_3.dat']
+files = ['squares_1.dat','squares_2.dat','squares_3.dat']
 for file in files:
     get_encomapsing_square(file)
